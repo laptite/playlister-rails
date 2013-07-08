f204a8cd51091bc182dafb5f74eea192c75cbdd7
laptite <laptite@gmail.com>
2013-07-08 12:59:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlaylisterRails::Application.config.secret_token = 'e6f8ac84bedb0da065e05cd785aa7660448dd1a19e7ea7d62caf3aa4b31f4ee058cb43c9b685ad04e39caf137e84082b46379e1ea9dcc4f962ec7b7db4da4979'
